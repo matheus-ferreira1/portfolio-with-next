d558bcf1a98aa147d2cfc85bcfe6ba83812e46b6
Matheus Ferreira <matheus.fer33@gmail.com>
2022-09-13 18:54:08 -0300

    added the projects component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to my <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor includes
+      </SectionText>
+      <Button>Download CV</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
