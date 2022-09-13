8903a8c244771af3f54fff252ebff0f758a5088f
Matheus Ferreira <matheus.fer33@gmail.com>
2022-09-13 19:08:51 -0300

    technologies section completed

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
-);
+const Technologies = () => {
+  return (
+    <Section id="tech">
+      <SectionDivider />
+      <br />
+      <SectionTitle>Technologies</SectionTitle>
+      <SectionText>
+        Experiência com projetos Front-End utilizando ferramentas como React,
+        Next, Angular e Vue
+      </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="3rem" />
+          <ListContainer>
+            <ListTitle>Front-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              React.JS
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiFirebase size="3rem" />
+          <ListContainer>
+            <ListTitle>Back-End</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              Node.js and databases
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiZend size="3rem" />
+          <ListContainer>
+            <ListTitle>UI/UX</ListTitle>
+            <ListParagraph>
+              Experience with <br />
+              Tools like Figma
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
+  );
+};
 
 export default Technologies;
